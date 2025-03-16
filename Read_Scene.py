c645064acd8112af8ec5f9d3e9bb7c573a22650e
Markus Fritze <github@sarnau.com>
2025-03-16 12:30:34 +0100

    String parsing improved for analyzing the scenes

--- Read_Scene.py
+++ Read_Scene.py
@@ -2,27 +2,9 @@
 # -*- coding: utf-8 -*-
 
 import os
+import sys
 import struct
 import loadObjectFile
-
-SUSPECTS = [
-	'Veronica Marlowe',
-	'Francis "Buck" Battle',
-	'Margaret Vandergilt',
-	'Vincent van Wente',
-	'Felicity Sucrose',
-	'Rod London',
-	'Sally Rose',
-	'Oswald Stonemann',
-	'Natalia Berenski',
-	'Reverend Jeremiah Folmuth',
-	'Stephie Hart-Winston',
-	'Aldo Sandini',
-	'Marie Roget',
-	'Anton Peste',
-	'Hester Prymme',
-	'Phillip Wollcraft'
-]
 
 def getString(data,offset):
 	s = ''
@@ -53,74 +35,16 @@ def getString(data,offset):
 			ch = ','
 		elif ch >= 0x80 and ch <= 0x8F:
 			ch = '<SUSPECT CALLS %s>' % SUSPECTS[ch & 0xF]
-		elif ch == 0x90:
-			ch = '<PRONOUN She/He>'
-		elif ch == 0x91:
-			ch = '<PRONOUN HER/HIS>'
-		elif ch == 0x92:
-			ch = '<PRONOUN her/him>'
-		elif ch == 0x93:
-			ch = '<PRONOUN she/he>'
-		elif ch == 0x94:
-			ch = '<PRONOUN her/his>'
-		elif ch == 0x95:
-			ch = '<PRONOUN husband/wife>'
-		elif ch == 0x96:
-			ch = '<PRONOUN Woman/man>'
-		elif ch == 0x97:
-			ch = '<PRONOUN Men/Women>'
-		elif ch == 0x98:
-			ch = '<PRONOUN He/She>'
-		elif ch == 0x99:
-			ch = '<PRONOUN he/she>'
-		elif ch == 0x9A:
-			ch = '<PRONOUN girl/fellow>'
 		elif ch == 0x9B: # Atari EOL
 			break
-		elif ch == 0x9C:
-			ch = '<PRONOUN man/woman>'
-		elif ch == 0x9D:
-			ch = '<PRONOUN him/her>'
-		elif ch == 0x9E:
-			ch = '<PRONOUN his/him>'
-		elif ch == 0x9F:
-			ch = '<PRONOUN Miss/Mr>'
+		elif (ch & 0xF0) == 0x90:
+			ch = '<PRONOUN %s/%s>' % (PRONOUNS[ch & 0xF],PRONOUNS[(ch & 0xF)+1])
 		elif ch >= 0xA0 and ch <= 0xAF:
 			ch = '<PERSON %s>' % SUSPECTS[ch & 0xF]
 		elif ch >= 0xB0 and ch <= 0xBF:
 			ch = '<PERSON %s>' % SUSPECTS[ch & 0xF]
-		elif ch == 0xC0:
-			ch = '<VERB is/was>'
-		elif ch == 0xC1:
-			ch = '<VERB s/d>'
-		elif ch == 0xC2:
-			ch = '<VERB s/ed>'
-		elif ch == 0xC3:
-			ch = '<VERB keeps/kept>'
-		elif ch == 0xC4:
-			ch = '<VERB \'s/was>'
-		elif ch == 0xC5:
-			ch = '<VERB oss/id>'
-		elif ch == 0xC6:
-			ch = '<VERB inks/ank>'
-		elif ch == 0xC7:
-			ch = '<VERB are/were>'
-		elif ch == 0xC8:
-			ch = '<VERB be/have been>'
-		elif ch == 0xC9:
-			ch = '<VERB can/could>'
-		elif ch == 0xCA:
-			ch = '<VERB s/>'
-		elif ch == 0xCB:
-			ch = '<VERB makes/made>'
-		elif ch == 0xCC:
-			ch = '<VERB gives/gave>'
-		elif ch == 0xCD:
-			ch = '<VERB will/would>'
-		elif ch == 0xCE:
-			ch = '<VERB have/had>'
-		elif ch == 0xCF:
-			ch = '<VERB have/had>'
+		elif (ch & 0xF0) == 0xC0:
+			ch = '<VERB %s/%s>' % (VERBS[ch & 0xF],VERBS[(ch & 0xF)+1])
 		elif ch == 0xD0:
 			ch = '<DETECTIVE SIR/MISS>'
 		elif ch == 0xD1:
@@ -142,10 +66,61 @@ def parseLSB_MSB_Text(data,baseAdr,size,lsbOffset,msbOffset):
 	lsbs = data[lsbOffset:lsbOffset+size]
 	msbs = data[msbOffset:msbOffset+size]
 	idx = 0
+	words = []
 	for lsb,msb in zip(lsbs,msbs):
 		offset = (msb << 8) + lsb
-		print('#%2d $%04x "%s"' % (idx, offset, getString(data,offset - baseAdr)))
+		s = getString(data,offset - baseAdr)
+		#print('#%2d $%04x "%s"' % (idx, offset, s))
 		idx += 1
+		words.append(s)
+	return words
+
+def parseLSB_Text(data,baseAdr,size,lsbOffset,msb):
+	lsbs = data[lsbOffset:lsbOffset+size]
+	idx = 0
+	words = []
+	for lsb in lsbs:
+		offset = (msb << 8) + lsb
+		s = getString(data,offset - baseAdr)
+		#print('#%2d $%04x "%s"' % (idx, offset, s))
+		idx += 1
+		words.append(s)
+	return words
+
+gamecode = bytearray(0x6DE) + bytearray(open('Murder on the Zinderneuf (1983)[a].atx.atboot','rb').read())
+
+# these need to be parsed first, because getString() might require them
+PRONOUNS = parseLSB_Text(gamecode,0,32,0x2e13,0x2e)
+VERBS = parseLSB_Text(gamecode,0,32,0x2e8c,0x2f)
+SUSPECTS = []
+suspect_firstname = parseLSB_Text(gamecode,0,17,0x1EB8,0x1F)
+suspect_lastname = parseLSB_Text(gamecode,0,17,0x1EC9,0x1F)
+for first,last in zip(suspect_firstname,suspect_lastname):
+	SUSPECTS.append('%s %s' % (first,last))
+
+room = getString(gamecode,0x1dad) # 'Room '
+rooms = parseLSB_Text(gamecode,0,19,0x1D63,0x1D) # The room names
+ROOMNAMES = []
+for idx in range(len(rooms)):
+	if idx == 0 or idx >= 17:
+		name = rooms[idx]
+	else: # the room numbers have a guest assigned to them
+		name = '%s%s: %s' % (room, rooms[idx], SUSPECTS[idx])
+	ROOMNAMES.append(name)
+
+sentence_start = parseLSB_Text(gamecode,0,16,0x2412,0x24)
+#print(sentence_start)
+sentence_end = parseLSB_Text(gamecode,0,16,0x24F1,0x25)
+#print(sentence_end)
+knows_nothing = parseLSB_Text(gamecode,0,16,0x2817,0x27)
+#print(knows_nothing)
+words = parseLSB_MSB_Text(gamecode, 0, 16, 0x2827, 0x2837)
+#print(words)
+words = parseLSB_MSB_Text(gamecode, 0, 16, 0x29d3, 0x29e3)
+#print(words)
+words = parseLSB_MSB_Text(gamecode, 0, 16, 0x2bc2, 0x2bd2)
+#print(words)
+#sys.exit(0)
 
 DIR = './Gamefiles/'
 for filename in sorted(os.listdir(DIR)):
@@ -161,13 +136,13 @@ for filename in sorted(os.listdir(DIR)):
 	scene = ret[loadAdr]
 	print(filename[:-4])
 	print('=' * (len(filename)-4))
-	parseLSB_MSB_Text(scene, 0x4800, 48, 0x0030, 0x0000)
+	print(parseLSB_MSB_Text(scene, 0x4800, 48, 0x0030, 0x0000))
 	print()
-	parseLSB_MSB_Text(scene, 0x4800, 24, 0x0078, 0x0060)
+	print(parseLSB_MSB_Text(scene, 0x4800, 24, 0x0078, 0x0060))
 	print()
 	print('Clues:')
-	parseLSB_MSB_Text(scene, 0x4800, 8, 0x0188, 0x0190)
+	print(parseLSB_MSB_Text(scene, 0x4800, 8, 0x0188, 0x0190))
 	print()
 	print('Confession:')
-	parseLSB_MSB_Text(scene, 0x4800, 8, 0x01B8, 0x01C0)
+	print(parseLSB_MSB_Text(scene, 0x4800, 8, 0x01B8, 0x01C0))
 	break
